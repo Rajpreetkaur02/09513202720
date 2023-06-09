6b43963567ed94f11a868fe32d63eb8f96ac315b
Rajpreet Kaur <kaurrajpreet2018@gmail.com>
2023-06-09 21:02:56 +0530

    new commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,34 @@
-import logo from './logo.svg';
 import './App.css';
+import { useEffect, useState } from 'react';
 
-function App() {
+export default function App() {
+  const [trains, settrains] = useState([]);
+  const headers = { 'Authorization': 'Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJleHAiOjE2ODYzMjQ5OTMsImNvbXBhbnlOYW1lIjoiR1RCSVQgQ2VudHJhbCIsImNsaWVudElEIjoiYTIwNmZiNzktZmU0Yy00NDFhLWEwOGUtMGQ0Y2FlZGUwYTBlIiwib3duZXJOYW1lIjoiIiwib3duZXJFbWFpbCI6IiIsInJvbGxObyI6IjA5NTEzMjAyNzIwIn0.l-ShEQ9UsH1Usrbt75obXL_0yTbnAqFvcFresoM3nQ4' }
+
+  useEffect(() => {
+
+    fetch('http://104.211.219.98/train/trains', { headers })
+      .then(response => {
+        response.json().then(train => {
+          settrains(train);
+        })
+      })
+  }, []);
+  console.log(trains);
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h2>Trains</h2>
+      {trains.map((item) => (
+        <>
+        <h2>{item.trainName}</h2>
+        <p>{item.trainNumber}</p>
+          <p>{item.departureTime.Hours}:{item.departureTime.Minutes}:{item.departureTime.Seconds}</p>
+        <p>{item.seatsAvailable.sleeper}:{item.seatsAvailable.AC}</p>
+        </>
+      ))}
+      
     </div>
   );
 }
 
-export default App;
+
